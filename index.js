323e81065e8b3fefeaefeafc4e8ff8101b4500f9
MAC <MAC@MacBook-Emilio.local>
2022-04-23 19:26:46 +0200

    exercici_2

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 "use strict";
-const acudits = document.getElementById("acudits");
-acudits.addEventListener(`click`, () => {
+const startButton = document.getElementById("startButton");
+const nextJokeButton = document.getElementById("nextJokeButton");
+const joke = document.getElementById("acudits");
+startButton.addEventListener(`click`, () => {
     fetch("https://icanhazdadjoke.com/", {
         method: "GET",
         headers: {
-            'Accept': 'application/json'
+            'Accept': 'application/json',
+            'User-Agent': 'My Library (https://github.com/username/repo)'
         }
     })
         .then(answer => answer.json())
-        .then(acudit => console.log(acudit.joke));
+        .then(acudit => {
+        console.log(acudit.joke);
+        const joke = document.getElementById("joke");
+        joke.textContent = `${acudit.joke}`;
+    });
+});
+nextJokeButton.addEventListener(`click`, () => {
+    fetch("https://icanhazdadjoke.com/", {
+        method: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'User-Agent': 'My Library (https://github.com/username/repo)'
+        }
+    })
+        .then(answer => answer.json())
+        .then(acudit => {
+        console.log(acudit.joke);
+        const joke = document.getElementById("joke");
+        joke.textContent = `${acudit.joke}`;
+    });
 });
--- index.ts
+++ index.ts
@@ -1,12 +1,37 @@
-const acudits=document.getElementById("acudits") as HTMLButtonElement
-acudits.addEventListener(`click`,()=>{
+const startButton=document.getElementById("startButton") as HTMLButtonElement
+const nextJokeButton=document.getElementById("nextJokeButton") as HTMLButtonElement
+const joke=document.getElementById("acudits") as HTMLButtonElement
+
+startButton.addEventListener(`click`,()=>{
     fetch("https://icanhazdadjoke.com/",{
         method:"GET",
         headers:{
-            'Accept': 'application/json'
+            'Accept': 'application/json',
+            'User-Agent': 'My Library (https://github.com/username/repo)'
         }
     })
     .then(answer=>answer.json())
-    .then(acudit=>console.log(acudit.joke))
+    .then(acudit=>{
+        console.log(acudit.joke)
+        const joke=document.getElementById("joke") as HTMLParagraphElement
+        joke.textContent=`${acudit.joke}` 
+    })
+
+})
+
+nextJokeButton.addEventListener(`click`,()=>{
+    fetch("https://icanhazdadjoke.com/",{
+        method:"GET",
+        headers:{
+            'Accept': 'application/json',
+            'User-Agent': 'My Library (https://github.com/username/repo)'
+        }
+    })
+    .then(answer=>answer.json())
+    .then(acudit=>{
+        console.log(acudit.joke)
+        const joke=document.getElementById("joke") as HTMLParagraphElement
+        joke.textContent=`${acudit.joke}` 
+    })
 
 })
