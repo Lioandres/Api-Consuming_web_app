7261e18350b00d7a30897e0406106dd6db1eb422
MAC <MAC@MacBook-Emilio.local>
2022-04-24 16:50:51 +0200

    exercici_3

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const startButton = document.getElementById("startButton");
 const nextJokeButton = document.getElementById("nextJokeButton");
 const joke = document.getElementById("acudits");
+let objectJoke;
+let reportAcudits = [];
 startButton.addEventListener(`click`, () => {
     fetch("https://icanhazdadjoke.com/", {
         method: "GET",
@@ -13,6 +15,11 @@ startButton.addEventListener(`click`, () => {
         .then(answer => answer.json())
         .then(acudit => {
         console.log(acudit.joke);
+        objectJoke = {
+            joke: acudit.id,
+            score: 1,
+            date: ""
+        };
         const joke = document.getElementById("joke");
         joke.textContent = `${acudit.joke}`;
     });
@@ -28,7 +35,25 @@ nextJokeButton.addEventListener(`click`, () => {
         .then(answer => answer.json())
         .then(acudit => {
         console.log(acudit.joke);
+        objectJoke = {
+            joke: acudit.id,
+            score: 1,
+            date: ""
+        };
         const joke = document.getElementById("joke");
         joke.textContent = `${acudit.joke}`;
     });
 });
+function toScore(score) {
+    if (score === 1)
+        objectJoke.score = 1;
+    if (score === 2)
+        objectJoke.score = 2;
+    if (score === 3)
+        objectJoke.score = 3;
+    const d = new Date();
+    objectJoke.date = d.toISOString();
+    if (!reportAcudits.find(element => element.joke === objectJoke.joke))
+        reportAcudits.push(objectJoke);
+    console.log(reportAcudits);
+}
--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 const startButton=document.getElementById("startButton") as HTMLButtonElement
 const nextJokeButton=document.getElementById("nextJokeButton") as HTMLButtonElement
 const joke=document.getElementById("acudits") as HTMLButtonElement
+interface jokes {joke:string; score:number;date:string}
+let objectJoke:jokes
+let reportAcudits:jokes[]=[]
 
 startButton.addEventListener(`click`,()=>{
     fetch("https://icanhazdadjoke.com/",{
@@ -13,6 +16,11 @@ startButton.addEventListener(`click`,()=>{
     .then(answer=>answer.json())
     .then(acudit=>{
         console.log(acudit.joke)
+        objectJoke={
+            joke:acudit.id,
+            score:1,
+            date:""
+        }
         const joke=document.getElementById("joke") as HTMLParagraphElement
         joke.textContent=`${acudit.joke}` 
     })
@@ -30,8 +38,31 @@ nextJokeButton.addEventListener(`click`,()=>{
     .then(answer=>answer.json())
     .then(acudit=>{
         console.log(acudit.joke)
+        objectJoke={
+            joke:acudit.id,
+            score:1,
+            date:""
+        }
         const joke=document.getElementById("joke") as HTMLParagraphElement
         joke.textContent=`${acudit.joke}` 
     })
 
 })
+
+
+function toScore(score:number):void{
+
+        if(score===1) objectJoke.score=1
+        if(score===2) objectJoke.score=2
+        if(score===3) objectJoke.score=3
+        const d=new Date();
+        objectJoke.date= d.toISOString();
+        if (!reportAcudits.find(element=>element.joke===objectJoke.joke)) reportAcudits.push(objectJoke)
+        console.log(reportAcudits)
+
+}        
+    
+
+
+
+
